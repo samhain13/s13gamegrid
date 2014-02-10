335fc66443dabdd2e54d0cf0dd3a89d71e0b046f
Arielle B Cruz <arielle.cruz@gmail.com>
2014-02-10 21:50:12 +0800

    Update: dev-demos/02
    Made a preliminary map of game properties which will be modified
    as the game progresses. Also added a couple more gridbox_types.

--- dev-demos/02/boxtypes.js
+++ dev-demos/02/boxtypes.js
@@ -1,24 +1,68 @@
 // The game grid controller requires this.
 var gb_default = new gridbox_type();
+gb_default.menu_items = [];
 gb_default.init = function (gb) {
-    // gb.box.css("border", "1px black solid");
     gb.set_image("res/gb-default.svg");
 };
-
-
-// Contains all player properties.
-var gb_home = new gridbox_type();
-gb_home.init = function(gb) {
-    // gb.box.css("border", "1px black solid");
-    gb.set_image("res/home.png", true);
-    // Assign some stuff to the player.
+gb_default.clicked = function(gb) {
+    // The show_menu function is defined in game.js.
+    show_menu("Build Menu", this.menu_items, gb);
+};
+gb_default.register = function(item_title, instance_name) {
+    // Register a gridbox_type instance as a Build Menu item.
+    this.menu_items.push([item_title,
+        "menu_box_selected.boxtype.build(menu_box_selected,"+instance_name+");"]);
+};
+gb_default.build = function(gb, gb_instance) {
+    // Show menu link target; builds buildings in place of this
+    // gridbox if the gb_instance requirements are satisfied.
+    var build_check = gb_instance.pre_init(gb);
+    if (build_check == true) gb.init(gb_instance);
+    else alert(build_check);
+    hide_menu();   // In game.js.
 };
 
-// Exit gridbox, shows the "explore" menu when clicked.
-var gb_exit = new gridbox_type();
-gb_exit.init = function(gb) {
-    gb.set_image("res/exit.png", true);
-}
-gb_exit.clicked = function(gb) {
-    alert("Explore, forage, or plunder.");
+
+// Contains all player and game properties.
+var gb_home = new gridbox_type();
+gb_home.init = function(gb) {
+    gb.set_image("res/home.png", true);
+};
+
+
+// Allows the player to assign people to explore the outside world.
+var gb_explore = new gridbox_type();
+gb_explore.init = function(gb) {
+    gb.set_image("res/explore.png", true);
+};
+
+
+
+// ------------- Buildings that can be built.
+var gb_windmill = new gridbox_type();
+gb_default.register("Windmill", "gb_windmill");
+gb_windmill.init = function(gb) {
+    gb.box.append($("<span>WINDMILL</span>"));
+};
+
+var gb_veg_garden = new gridbox_type();
+gb_default.register("Vegetable Garden", "gb_veg_garden");
+gb_veg_garden.init = function(gb) {
+    gb.set_image("res/veg-garden.png", true);
+    gb.dict = {
+        "fruits":     0,
+        "vegetables": 0,
+        "water":      0,
+    };
+};
+gb_veg_garden.update = function(gb) {
+    // Updates every 06:00.
+    if (game_stats["game time"]["hour"] == "06:00") {
+        // Here.
+    }
+};
+gb_veg_garden.clicked = function(gb) {
+    menu_items = [];
+    // Do some checks before adding menu items to limit
+    // what the player can do.
 };
--- dev-demos/02/game.js
+++ dev-demos/02/game.js
@@ -1,32 +1,160 @@
+var seconds_per_turn = 10;      // Basically, setInterval value * 1000
 var home_box = null;
+var menu_box_selected = null;
+var game_interval = null;
+
+// Set the player's and game stats here.
+var game_stats = {
+    "game time": {
+        "day":           1,
+        "hour":    "07:00",
+    },
+    "essentials": {
+        "control":      100,    // game over when 0 or less
+        "electricity":    0,    // supplied by batteries
+        "rations":       28,    // supplied by food tins
+        "water":          0,    // supplied by water jugs
+    },
+    "people": {
+        // Different types of people give bonuses to different gridbox_types.
+        "refugees":       1,    // stock person type
+    },
+    "other supplies": {
+        "ammunition":     0,    // for security/combat
+        "batteries":      0,    // for storing energy
+        "food tins":      0,    // for making rations
+        "water bottles":  0,    // for getting water
+    },
+};
+
+
+function hide_menu() {
+    menu_box_selected.remove_image("res/selected-box.png");
+    menu_box_selected = null;
+    $("#gridbox-menu").html("");
+}
+
+function show_menu(title, items, gb) {
+    if (menu_box_selected != null) hide_menu();
+    menu_box_selected = gb;
+    menu_box_selected.set_image("res/selected-box.png");
+    var m = $("#gridbox-menu");
+    m.html("");
+    m.append($("<h3>" +title+ "</h3>"));
+    $.each(items, function() {
+        var a = $("<a></a>");
+        a.text(this[0]);
+        a.attr("href", "javascript:" + this[1]);
+        m.append(a);
+    });
+    m.append($('<a>[x]</a>'));
+    $("#gridbox-menu a:last-child").click( function() { hide_menu(); });
+}
 
 function show_stats() {
-    var table = $("#menu table");
-    $.each(home_box.dict["keys"], function() {
-        /*
+    var table = $("#disp-stats");
+    table.html("");
+    $.each(Object.keys(game_stats), function() {
         var tr = $("<tr></tr>");
-        tr.append($("<td>" +this.toString()+ "</td>"));
-        tr.append($("<td>" +home_box.dict.get(this.toString())+ "</td>"));
-        table.append(tr);
-        */
+        var v = game_stats[this];
+        if (v instanceof String || v instanceof Number) {
+            tr.append($("<td>" +title(this.toString())+ "</td>"));
+            tr.append($("<td>" +v+ "</td>"));
+            table.append(tr);
+        } else {
+            tr.append($('<td colspan="2">' +title(this)+ '</td>'));
+            table.append(tr);
+            $.each(Object.keys(v), function() {
+                var tr2 = $("<tr></tr>");
+                tr2.append($("<td>" +title(this)+ "</td>"));
+                if (isNaN(v[this])) tr2.append($("<td>" +v[this]+ "</td>"));
+                else tr2.append($("<td>" +Math.round(v[this])+ "</td>"));
+                table.append(tr2);
+            });
+        }
     });
 }
+
+// ------------- All update functions come after this one.
+function game() {
+    // This is the game's "main" function, which is called every x seconds.
+    // ------------- Update the player and game stats first.
+    update_time();
+    // ------------- Consume resources.
+    var consumers = 0;
+    $.each(Object.keys(game_stats["people"]), function() {
+        consumers += game_stats["people"][this];
+    });
+    if (["06:00", "18:00"].indexOf(game_stats["game time"]["hour"]) >= 0) {
+        update_resource(consumers, "rations", "food tins");
+        update_resource(consumers, "water", "water jugs");
+    }
+    // ------------- Update every other box but home_box because we've done that.
+    $.each(stage.grid, function() {
+        if (this != home_box) this.boxtype.update(this);
+    });
+    //------------- Finally, update the display.
+    show_stats();
+}
+
+function pause_game() {
+    if (game_interval == null) {
+        game_interval = setInterval(game, seconds_per_turn * 1000);
+    } else {
+        clearInterval(game_interval);
+        game_interval = null;
+    }
+}
+
+function update_control(consumers, reason) {
+    // Computes how much control the player loses when things happen.
+    if (reason == "food") var d = 0.1;    // Some arbitrary score for loss hunger.
+    else var d = 0.1;                     // Default score.
+    if (consumers > 1) {
+        gb.dict["essentials"]["control"] -= consumers * d;
+        // If control goes below 1, reduce the number of people.
+    }
+}
+
+function update_resource(consumers, resource, byproduct) {
+    // Computes how much essential resources will be used and how
+    // much supplies will be recycled because of the consumption.
+    var c = game_stats["essentials"][resource] - consumers;
+    if (c >= 0) {
+        if (byproduct != undefined) game_stats["other supplies"][byproduct] +=
+            (game_stats["essentials"][resource] - c);
+        game_stats["essentials"][resource] = c;
+    } else {
+        game_stats["essentials"][resource] = 0;
+        // Compute how much control the player loses over the settlement.
+        update_control(consumers, resource);
+    }
+}
+
+function update_time() {
+    var gmt = game_stats["game time"];
+    var h = parseInt(gmt["hour"].split(":")[0]) + 1;
+    if (h == 24) { h = 0; gmt["day"] += 1; }
+    if (h < 10) h = "0" + h;
+    gmt["hour"] = h + ":00";
+    // Roll for random event?
+}
+
 
 $(document).ready( function() {
     // Create the grid.
     stage.init(8, 40);
     stage.set_image("res/stage.jpg");
-    $("#menu, #stage").height($("#grid").height());
-    
-    // Set the home box.
+    // Set the home box; this is where people sleep.
     home_box = stage.grid[11];
     home_box.init(gb_home);
-    
     // Set the exit boxes so the player can explore, forage, and plunder.
     // This is also where enemies might come in.
-    stage.grid[23].init(gb_exit);
-    stage.grid[32].init(gb_exit);
-    
-    // Build the stats display.
+    stage.grid[23].init(gb_explore);
+    stage.grid[32].init(gb_explore);
+    // And give the player a source of food and water.
+    stage.grid[12].init(gb_veg_garden);
     show_stats();
+    // Start the game.
+    pause_game();
 });
